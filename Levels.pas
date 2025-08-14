2adbfd9efc5db13db5a94cf96fd0c3d376b3e251
EgorIchi <egorpushkarev996@gmail.com>
2025-08-14 10:23:43 +0300

    Add files via upload

---
+++ Levels.pas
@@ -0,0 +1,175 @@
+﻿Unit Levels;
+
+interface
+type
+Person=record
+  first_name:string[50];
+  last_name:string[50];
+end;
+var f:file of Person;
+
+
+procedure Easy_Level(man:Person);
+procedure Medium_Level(man:Person);
+procedure Hard_Level(man:Person);
+
+implementation
+
+procedure Easy_Level(man:Person);
+var count_life:integer;
+m:Person;
+min:integer;
+max:integer;
+secret_number:integer;
+you_number:integer;
+begin
+  count_life:=10;
+  writeln;
+  writeln(man.first_name,' ',man.last_name,' Its easy level you have ',count_life,' tries');
+  writeln;
+  Write('Write the minimum number:');
+  Readln(min);
+  Write('Write the maximum number:');
+  Readln(max);
+  writeln;
+  secret_number:=Random(min,max);
+
+  while true do
+  begin
+    Writeln('Attempt number:',count_life);
+    Write('Write your number:');
+    Readln(you_number);
+    
+    if you_number>secret_number then writeln('Write the number less')
+    else if you_number<secret_number then writeln('Write the number more')
+    
+   else  if count_life=0 then begin
+      writeln;
+      writeln('Sorry!!! ',man.first_name,' ', man.last_name);
+      Writeln('You lose and you have:',count_life,' Attempt');
+      writeln;
+      break;
+      end
+   else if you_number=secret_number then begin
+      Writeln;
+      Writeln('Congratulations!!! ',man.first_name,' ', man.last_name);
+      Writeln('You win this game');
+      Writeln('The Secret number is:',secret_number);
+      Writeln('Your quantity attempt is:',count_life);
+      Writeln;
+      
+      break;
+    end;
+    
+    count_life:=count_life-1;
+    Writeln;
+  end;
+end;
+
+
+procedure Medium_Level(man:Person);
+var count_life:integer;
+min:integer;
+max:integer;
+secret_number:integer;
+you_number:integer;
+begin
+  count_life:=7;
+  writeln;
+  writeln(man.first_name,' ',man.last_name,' Its medium level you have ',count_life,' tries');
+  writeln;
+  Write('Write the minimum number:');
+  Readln(min);
+  Write('Write the maximum number:');
+  Readln(max);
+  writeln;
+  secret_number:=Random(min,max);
+  while true do
+  begin
+    Writeln('Attempt number:',count_life);
+    Write('Write your number:');
+    Readln(you_number);
+    
+    if count_life =0 then 
+    begin
+      writeln;
+      writeln('Sorry!!! ',man.first_name,' ', man.last_name);
+      Writeln('You lose and you have:',count_life,' Attempt');
+      writeln;
+      break;
+    end
+    else if you_number=secret_number then
+    begin
+        Writeln;
+      Writeln('Congratulations!!! ',man.first_name,' ', man.last_name);
+      Writeln('You win this game');
+      Writeln('The Secret number is:',secret_number);
+      Writeln('Your quantity attempt is:',count_life);
+      Writeln;
+      break;
+    end
+    else if you_number<> secret_number then begin
+      writeln('Wrong!!!');
+      writeln;
+    end;
+    count_life:=count_life-1;
+  end;
+end;
+
+
+procedure Hard_Level(man:Person);
+var count_life:integer;
+q:Person;
+min:integer;
+max:integer;
+secret_number:integer;
+you_number:integer;
+begin
+  count_life:=5;
+  writeln;
+  writeln(man.first_name,' ',man.last_name,' Its hard level you have ',count_life,' tries');
+  writeln;
+  Write('Write the minimum number:');
+  Readln(min);
+  Write('Write the maximum number:');
+  Readln(max);
+  writeln;
+  while true do
+  begin
+    secret_number:=Random(min,max);
+    Writeln('Attempt number:',count_life);
+    Write('Write your number:');
+    Readln(you_number);
+    if you_number=secret_number then
+    begin
+      Writeln;
+      Writeln('Congratulations!!! ',man.first_name,' ', man.last_name);
+      Writeln('You win this game');
+      Writeln('The Secret number is:',secret_number);
+      Writeln('Your quantity attempt is:',count_life);
+      Writeln;
+      
+      break;
+    end 
+    else if count_life=0 then begin
+      writeln;
+      writeln('Sorry!!! ',man.first_name,' ', man.last_name);
+      Writeln('You lose and you have:',count_life,' Attempt');
+      writeln;
+      break;
+     
+    end
+    else if you_number<> secret_number then begin
+      writeln('Wrong!!!');
+      writeln;
+    end;
+    
+    
+    count_life:=count_life-1;
+    
+  end;
+end;
+
+begin
+
+end.
---
+++ program.pas
@@ -0,0 +1,43 @@
+﻿program Guess_Numb;
+uses Levels;
+uses crt;
+var level:string;
+Per:Person;
+begin
+  Writeln('Game - "Guess the number"');
+  writeln;
+  
+  Write('Input your first name:');
+  Readln(Per.first_name);
+  Write('Input your last name:');
+  Readln(Per.last_name);
+  writeln;
+  writeln('Hello!!! Welcome to the game guess the number: ',Per.first_name,' ', Per.last_name);
+  writeln;
+  
+  
+  repeat
+   
+   writeln('Select and write your level:');
+   writeln('Easy - 10 tries and tips');
+   writeln('Medium - 7 tries and without tips');
+   writeln('Hard - 5 tries and the number replace every time');
+   writeln('Exit - Exit the programm');
+   
+   
+ 
+   writeln;
+   Write('Write your level:');
+   Readln(level);
+     case level of 
+     'Easy': begin
+       Easy_Level(Per)
+     end;
+     'Medium': Medium_Level(Per);
+     'Hard': Hard_Level(Per);
+    
+   end;
+   until level = 'Exit' ;
+  
+  
+end.
